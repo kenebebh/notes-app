52a28058d1f999491e1cb0da75cdaea51ce1f34f
kenebebh <kenebebhbanigo@gmail.com>
2025-07-20 23:34:29 +0100

    controller to log user out and destroy cookie

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -63,7 +63,12 @@ const registerUser = asyncHandler(async (req, res, next) => {
 //Access: Public
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User route" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out user" });
 });
 
 //Description: Get User profie
